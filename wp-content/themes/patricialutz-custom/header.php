71a677a8932762cab14750723a6e2e1dc1ca4dad
Pea <pea@misfist.com>
2014-11-10 00:32:49 -0500

    Added viewport meta for mobile.

--- wp-content/themes/patricialutz-custom/header.php
+++ wp-content/themes/patricialutz-custom/header.php
@@ -7,7 +7,10 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="initial-scale=1">
+
 	<title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
+	
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
