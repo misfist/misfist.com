efa46880987d6ab02007cdfd86362a1fd62ffcea
Pea <pea@misfist.com>
2015-09-27 00:28:31 -0400

    Updated Jetpack

--- wp-content/themes/chunk/inc/jetpack.php
+++ wp-content/themes/chunk/inc/jetpack.php
@@ -10,11 +10,34 @@
  * Add theme support for Infinite Scroll.
  * See: http://jetpack.me/support/infinite-scroll/
  */
-function chunk_jetpack_setup() {
-	add_theme_support( 'infinite-scroll', array(
-		'footer_widgets' => is_active_sidebar( 'sidebar-1' ),
-		'container'      => 'content',
-		'footer'         => 'page',
-	) );
+add_action( 'infinite_scroll_render', 'chunk_infinite_scroll_render' );
+add_action( 'init',                   'chunk_infinite_scroll_init' );
+
+/**
+ * Add theme support for infinity scroll
+ */
+function chunk_infinite_scroll_init() {
+	// Theme support takes one argument: the ID of the element to append new results to.
+	add_theme_support( 'infinite-scroll', 'contents' );
 }
-add_action( 'after_setup_theme', 'chunk_jetpack_setup' );
+
+/**
+ * Set the code to be rendered on for calling posts,
+ * hooked to template parts when possible.
+ *
+ * Note: must define a loop.
+ */
+function chunk_infinite_scroll_render() {
+	while ( have_posts() ) : the_post();
+		get_template_part( 'content', get_post_format() );
+	endwhile;
+}
+
+/**
+ * Do we have footer widgets?
+ */
+function infinite_scroll_has_footer_widgets() {
+	if ( is_active_sidebar( 'sidebar-1' ) )
+		return true;
+	return false;
+}
