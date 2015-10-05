8312098796fc7db43274d12b4a973ae779a252a8
Pea <pea@misfist.com>
2015-10-05 17:14:12 -0400

    Added new gridz theme.

---
+++ wp-content/themes/gridz/content-none.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package gridz
+ */
+?>
+<article id="post-0" class="post">
+    <header class="entry-header">
+        <h1 class="entry-title"><?php _e('No posts found here','gridz'); ?></h1>
+    </header>
+    <div class="entry-content">
+        <p><?php _e('Post has either been removed or does not exist. Please try using the search','gridz'); ?></p>
+    </div>
+</article>
