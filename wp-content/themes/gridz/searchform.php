8ede91897705e0a6f0366c6df9352b175014d8a0
Pea <pea@misfist.com>
2015-11-26 15:56:26 -0500

    Remove all files

--- wp-content/themes/gridz/searchform.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package gridz
- */
-?>
-<form method="get" id="searchform" action="<?php echo home_url('/'); ?>">
-    <input type="text" value="" name="s" id="s"/>
-    <input type="submit" id="searchsubmit" value="<?php _e('Search','gridz'); ?>" />
-</form>
--- wp-content/themes/patricia-lutz/partials/section-as-seen-on.php
+++
@@ -1,5 +0,0 @@
-<h2 id="eye" class="sectionHead">Title</h2>
-
-<ul id="seenOn">
-	<li class="bwWrapper">image</li> 
-</ul><!--end seenOn-->
